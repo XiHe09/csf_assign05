533beb23ab08a48d54d3f40fe6cbc31c84f806f5
Xi (Bella) He <49040083+XiHe09@users.noreply.github.com>
2020-11-19 02:00:08 -0500

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include "calc.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::map;
